4286501b7bb49556f5b29188863d51007ee4d42f
Sarb Bhinder <sarb8275@gmail.com>
2023-01-13 17:24:55 -0500

    model updated

--- main_app/models.py
+++ main_app/models.py
@@ -31,13 +31,6 @@ class Product(models.Model):
         # Nice method for obtaining the friendly value of a Field.choice
         return f' product name : {self.name}, Category :{self.get_category_display()}, Price : {self.price}'
 
-class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity =  models.IntegerField(default=1)
-
-    def __str__(self):
-        return str(self.product_id)
 
 class Photo(models.Model):
     url = models.CharField(max_length=200)
@@ -47,19 +40,19 @@ class Photo(models.Model):
          
         return f"Photo for cat_id: {self.product_id} @{self.url}"
 
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity =  models.IntegerField(default=1)
 
-# class Cart(models.Model):
-#     name = models.CharField(max_length=50)
-#     description = models.CharField(max_length=600)
-#     price = models.DecimalField(max_digits=19, decimal_places=10)
-#     category =  models.CharField(
-#             max_length=1,
-#             # add the 'choices' field option
-#             choices=CATEGORY,
-#             # set the default value for meal to be 'B'
-#             default=CATEGORY[0][0]
-#         )
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f'{self.id}'
 
-#     def __str__(self):
-#         # Nice method for obtaining the friendly value of a Field.choice
-#         return f'{self.get_category_display()}'
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cart = models.ManyToManyField(Cart)
+
+    def __str__(self):
+        return str(self.product_id)
